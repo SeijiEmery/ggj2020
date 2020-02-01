dbecc139dfb0425ed2bbd92a910292680b70de54
Seiji Emery <seijiemery@gmail.com>
2020-02-01 01:17:30 -0800

    init application

--- source/app.d
+++ source/app.d
@@ -1,6 +1,21 @@
 import std.stdio;
+import std.string: toStringz;
 import raylib;
 
 void main() {
-	
+    const auto screenSize = Vector2(800, 600);
+	writefln("%s", screenSize);
+
+	InitWindow(cast(int)screenSize.x, cast(int)screenSize.y, "ggj 2020");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		// UPDATE
+
+		// DRAW
+		BeginDrawing();
+			ClearBackground(RAYWHITE);
+			DrawText("Hello, world!".toStringz, 190, 200, 20, LIGHTGRAY);
+		EndDrawing();
+	}
 }
